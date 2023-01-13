9686630c67686ba4d42e9042b087efc87d5b3b97
lukazeljkovic <57068745+lukazeljkovic@users.noreply.github.com>
2023-01-13 12:20:18 +0100

    Frontend added

--- API/Program.cs
+++ API/Program.cs
@@ -14,7 +14,16 @@ builder.Services.AddDbContext<DataContext> (opt =>
     opt.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
 });
 
+builder.Services.AddCors(opt => {
+    opt.AddPolicy("CorsPolicy", policy => 
+    {
+        policy.AllowAnyMethod().AllowAnyHeader().WithOrigins("http://localhost:3000");
+    });
+});
+
 var app = builder.Build();
+
+
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
@@ -22,6 +31,8 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
+
+app.UseCors("CorsPolicy");
 
 
 app.UseAuthorization();
